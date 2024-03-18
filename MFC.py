3b585344e614533eff0067a1dbc35a0a4f247c86
Parthav Patel <patep62@mcmaster.ca>
2024-03-18 19:46:27 -0400

    MFC module

---
+++ MFC.py
@@ -0,0 +1,21 @@
+import serial
+
+class MFC_device:
+    
+    def __init__(self, comPort):
+        self.port = serial.Serial(port=comPort, baudrate=9600, bytesize=8 , timeout=.1, parity=serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE )
+
+    def setFlowRate(self, port, value):
+        cmd = 'AZ.' + port + 'P01=' + value + '\r' 
+        self.port.write(cmd.encode())
+        response = self.port.readline()
+        print(response)
+
+    def getFlowRate(self, port):
+        cmd = 'AZ.' + port + 'P01?\r' 
+        self.port.write(cmd.encode())
+        response = self.port.readline()
+        print(response)
+
+
+   
